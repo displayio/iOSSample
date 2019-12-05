c4263594660e618145f4315bdede19739ec8baa4
Ariel Malka <ariel@chronotext.org>
2019-12-05 18:40:58 +0200

    Removing sections in the table as it clashes with Feed Interstitial header

--- iOSSample/FeedViewController.m
+++ iOSSample/FeedViewController.m
@@ -14,7 +14,7 @@
 
 @interface FeedViewController ()
 
-@property (nonatomic, strong) NSArray<NSArray<NSNumber*>*> *ids;
+@property (nonatomic, strong) NSArray<NSNumber*> *ids;
 
 @end
 
@@ -27,7 +27,7 @@
     [self.tableView registerClass:[CustomCell2 class] forCellReuseIdentifier:@"IDENTIFIER2"];
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"IDENTIFIER3"];
     
-    self.ids = @[@[@1, @0, @1], @[@2, @0, @1], @[@0, @1, @0]];
+    self.ids = @[@1, @0, @1, @0, @1, @2, @0, @1, @0, @1, @0];
     
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Close" style:UIBarButtonItemStylePlain target:self action:@selector(close:)];
     
@@ -41,28 +41,15 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return [self.ids count];
-}
-
-- (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    switch (section) {
-        case 0:
-            return @"SECTION 1";
-        case 1:
-            return @"SECTION 2";
-        case 2:
-            return @"SECTION 3";
-    }
-    
-    return nil;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.ids[section] count];
+    return [self.ids count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    switch ([self.ids[indexPath.section][indexPath.row] intValue])
+    switch ([self.ids[indexPath.row] intValue])
     {
         case 0:
         {
@@ -97,13 +84,13 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    switch ([self.ids[indexPath.section][indexPath.row] intValue])
+    switch ([self.ids[indexPath.row] intValue])
     {
         case 0:
             return 150;
             
         case 1:
-            return 100;
+            return 120;
             
         case 2:
             if (self.ad.isInFeed) {
